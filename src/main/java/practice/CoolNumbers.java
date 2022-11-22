18edf7feecf1c0533877624acef9a292a62563c4
Independent <drugovh@mail.ru>
2022-11-22 15:34:28 +0200

    10.13 homework

--- src/main/java/practice/CoolNumbers.java
+++ src/main/java/practice/CoolNumbers.java
@@ -1,30 +1,91 @@
 package practice;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        String[] XYZ = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+        String[] N = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] R = new String[198];
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        for (int i = 0; i <= R.length - 1; i++) {
+            if (i <= 8) {
+                R[i] = "0" + (i + 1);
+            } else R[i] = String.valueOf(i + 1);
+        }
+        List<String> coolNumber = new ArrayList<>();
+        while (!(e == R.length)) {
+            String number = XYZ[a] + N[b] + N[b] + N[b] + XYZ[c] + XYZ[d] + R[e];
+            coolNumber.add(number);
+            ++a;
+            if (a == XYZ.length) {
+                a = 0;
+                c++;
+            }
+            if (c == XYZ.length) {
+                c = 0;
+                d++;
+            }
+            if (d == XYZ.length) {
+                d = 0;
+                b++;
+            }
+            if (b == N.length) {
+                b = 0;
+                e++;
+            }
+        }
+        return coolNumber;
+    }
+
+    public List<String> getGenerateCoolNumbers() {
+        return generateCoolNumbers();
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+        List<String> listNumber = new ArrayList<>(list);
+        for (String getNumber : listNumber) {
+            if (getNumber.equals(number)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public boolean getBruteForceSearchInList(List<String> list, String number) {
+        return bruteForceSearchInList(list, number);
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        return Collections.binarySearch(sortedList, number) >= 0;
+    }
+
+    public boolean getBinarySearchInList(List<String> sortedList, String number) {
+        return binarySearchInList(sortedList, number);
     }
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        HashSet<String> setNumber = new HashSet<>(hashSet);
+        return setNumber.contains(number);
+
+    }
+
+    public boolean getSearchInHashSet(HashSet<String> hashSet, String number) {
+        return searchInHashSet(hashSet, number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        TreeSet<String> setNumber = new TreeSet<>(treeSet);
+        return setNumber.contains(number);
+    }
+
+    public boolean getSearchInTreeSet(TreeSet<String> treeSet, String number) {
+        return searchInTreeSet(treeSet, number);
     }
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,14 +1,41 @@
 package practice;
 
+import java.util.*;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
-
     public static void main(String[] args) {
+        CoolNumbers coolNumbers = new CoolNumbers();
+        List<String> list = new ArrayList<>(coolNumbers.getGenerateCoolNumbers());
 
+        long start = System.nanoTime();
+        boolean bruteForceSearchInList = coolNumbers.getBruteForceSearchInList(list, "Х999ВЕ196");
+        long end = System.nanoTime() - start;
+        if (bruteForceSearchInList) {
+            System.out.println("Поиск перебором: найден, поиск занял: " + end + "нс");
+        } else System.out.println("Поиск перебором: не найден, поиск занял: " + end + "нс");
+
+        HashSet<String> hashSet = new HashSet<>(list);
+        start = System.nanoTime();
+        boolean searchInHashSet = coolNumbers.getSearchInHashSet(hashSet, "Х999ВЕ196");
+        end = System.nanoTime() - start;
+        if (searchInHashSet) {
+            System.out.println("Поиск в HashSet: найден, поиск занял: " + end + "нс");
+        } else System.out.println("Поиск в HashSet: не найден, поиск занял: " + end + "нс");
+
+        TreeSet<String> treeSet = new TreeSet<>(list);
+        start = System.nanoTime();
+        boolean searchInTreeSet = coolNumbers.getSearchInTreeSet(treeSet, "Х999ВЕ196");
+        end = System.nanoTime() - start;
+        if (searchInTreeSet) {
+            System.out.println("Поиск в TreeSet: найден, поиск занял: " + end + "нс");
+        } else System.out.println("Поиск в TreeSet: не найден, поиск занял: " + end + "нс");
+
+        Collections.sort(list);
+        start = System.nanoTime();
+        boolean binarySearchInList = coolNumbers.getBinarySearchInList(list, "Х999ВЕ196");
+        end = System.nanoTime() - start;
+        if (binarySearchInList) {
+            System.out.println("Бинарный поиск: найден, поиск занял: " + end + "нс");
+        } else System.out.println("Бинарный поиск: не найден, поиск занял: " + end + "нс");
     }
 }
